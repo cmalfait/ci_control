0b119a59874b58f3420583d3aba21f1328dbd33f
Chad Malfait <chad.malfait@autodesk.com>
2014-08-24 10:18:08 -0600

    Removed debian build greom sensu-ruby

--- sensu-ruby/00_control/debian/collectd.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the collectd package
-#
-0 4	* * *	root	[ -x /usr/bin/collectd_maintenance ] && /usr/bin/collectd_maintenance
--- sensu-ruby/00_control/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(collectd):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="collectd" command="/usr/bin/collectd"
